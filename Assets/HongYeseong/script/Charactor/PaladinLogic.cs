918324335d58ace71f0d0492387f0b1d5b5ac966
hongyeseong <eunpyohong44@gmail.com>
2026-05-27 20:25:54 +0900

    ㅁ

--- Assets/HongYeseong/script/Charactor/PaladinLogic.cs
+++ Assets/HongYeseong/script/Charactor/PaladinLogic.cs
@@ -17,10 +17,9 @@ public class PaladinLogic : MonoBehaviour, ICharacterSkill
         {
             case "Defense_Boost":
                 Defense_Boost(prefabInfo);
-                DisableAfterDuration(prefabInfo, prefabInfo.duration, prefabInfo.defense, 0, 0, 0);
                 break;
             case "SlowDown":
-                DisableAfterDuration(prefabInfo, prefabInfo.duration, 0, 0, prefabInfo.speed, 0 );
+                SlowDown(prefabInfo);
                 break;
             case "Default_Slash":
                 
@@ -32,18 +31,9 @@ public class PaladinLogic : MonoBehaviour, ICharacterSkill
     {
         charaStat.characterStats.defense += prefabInfo.defense;
     }
-    private IEnumerator DisableAfterDuration(PrefabInfo prefabInfo, float duration, float defense, float cooldown, float speed, float power)
+    
+    private void SlowDown(PrefabInfo prefabInfo)
     {
-        yield return new WaitForSeconds(duration);
-
-        if (prefabInfo != null)
-        { 
-            charaStat.characterStats.defense -= defense;
-            charaStat.characterStats.duration -= duration;
-            charaStat.characterStats.cooldown -= cooldown;
-            charaStat.characterStats.projectileSpeed += speed;
-            charaStat.characterStats.power -= power;
-            PoolManager.Instance.Release(prefabInfo.skillData.skillId, prefabInfo.gameObject);
-        }
-    }
+        
+    }   
 }
